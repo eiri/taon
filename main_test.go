d728da26ba90555bf379cb1e538ca82b9d88021f
Eric Avdey <eiri@eiri.ca>
2018-07-03 10:42:57 -0300

    Support parsing of none-object arrays

--- main_test.go
+++ main_test.go
@@ -20,13 +20,26 @@ var headertests = []headertestpair{
 
 // TestMakeHeader to ensure we are getting sorted list of strings
 func TestMakeHeader(t *testing.T) {
-	in := map[string]int{"b": 1, "z": 2, "a": 3, "r": 4, "o": 5}
+	in := map[string]interface{}{"b": 1, "z": 2, "a": 3, "r": 4, "o": 5}
 	for _, pair := range headertests {
 		columns = pair.columns
-		out := makeHeader(in)
+		out, err := makeHeader(in)
+		if err != nil {
+			t.Fatal(err)
+		}
 		if !reflect.DeepEqual(pair.expect, out) {
 			t.Errorf("Expecting %#v, got %#v", pair.expect, out)
 		}
+	}
+	// return error
+	columns = &ColumnsValue{"none"}
+	out, err := makeHeader(in)
+	if err == nil {
+		t.Error("Expecting error, got nil")
+	}
+	var expect Header
+	if !reflect.DeepEqual(expect, out) {
+		t.Errorf("Expecting %#v, got %#v", expect, out)
 	}
 }
 
@@ -70,6 +83,35 @@ func TestParseJSONArray(t *testing.T) {
 	for i, l := range "abcde" {
 		row := []string{strconv.Itoa(i + 1), string(l)}
 		expectRows = append(expectRows, row)
+	}
+	if !reflect.DeepEqual(expectRows, rows) {
+		t.Errorf("Expecting %#v, got %#v", expectRows, rows)
+	}
+}
+
+// TestParseMiscJSONArray to ensure we can parse arbitrary JSON array
+func TestParseMiscJSONArray(t *testing.T) {
+	columns = &ColumnsValue{}
+	r, err := os.Open("testdata/misc-array.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	header, rows, err := parseJSON(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectHeader := Header{"value"}
+	if !reflect.DeepEqual(expectHeader, header) {
+		t.Errorf("Expecting %#v, got %#v", expectHeader, header)
+	}
+	expectRows := Rows{
+		[]string{"309"},
+		[]string{"true"},
+		[]string{"zG8dnbd1iXDHAewJ"},
+		[]string{"false"},
+		[]string{"773"},
+		[]string{"Og3TQltUz2eIW6ZF"},
+		[]string{"map[note:c#]"},
 	}
 	if !reflect.DeepEqual(expectRows, rows) {
 		t.Errorf("Expecting %#v, got %#v", expectRows, rows)
